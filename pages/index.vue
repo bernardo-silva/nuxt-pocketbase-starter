7b59240c62b4e16036479bc53e2ef2ab4da0dd4a
Bernardo Silva <bernardofasilva@protonmail.com>
2025-02-16 10:42:49 +0000

    Better landing page

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,17 @@ const modalStore = useModalStore();
 
 </script>
 <template>
-  <h1>HELLO THERE</h1>
-  <button class="btn" @click="modalStore.open('Test error')">Show error</button>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello there
+          <span class="animate-pulse rotate-90 hover:-rotate-45 transition inline-block">👋</span>
+        </h1>
+        <p class="py-6">
+          This is a very good website that you can use for stuff
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
 </template>
